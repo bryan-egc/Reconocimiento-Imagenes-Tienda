971d476ad58de4b7b2f3c3fedcce98e102e89e1d
unknown <bg6907992@gmail.com>
2024-06-01 08:41:57 -0600

    Limitando clases

--- TiendaIA.py
+++ TiendaIA.py
@@ -21,14 +21,9 @@ class ShopIA:
         # CLASES:
         # Objects
         #clsObject = ObjectModel.names
-        clsObject = ['person','bicycle','car','motorcycle','airplane','bus','train','truck','boat','traffic light',
-                     'fire hydrant','stop sign','parking meter','bench','bird','cat','dog','horse','sheep','cow','elephant',
-                     'bear','zebra','giraffe','backpack','umbrella','handbag','tie','suitcase','frisbee','skis','snowboard',
-                     'sports ball','kite','baseball bat','baseball glove','skateboard','surfboard','tennis racket','bottle',
-                     'wine glass','cup','fork','knife','spoon','bowl','banana','apple','sandwich','orange','broccoli','carrot',
-                     'hot dog','pizza','donut','cake','chair','couch','potted plant','bed','dining table','toilet','tv','laptop',
-                     'mouse','remote','keyboard','cell phone','microwave','oven','toaster','sink','refrigerator','book','clock','vase',
-                     'scissors','teddy bear','hair drier','toothbrush']
+        clsObject = ['bottle','cup','fork','knife','spoon','banana','apple',
+                     'orange','broccoli','carrot','hot dog','pizza','donut',
+                     'mouse','keyboard','cell phone','book','clock','scissors','toothbrush']
         self.clsObject = clsObject
 
         # Bills Bank
@@ -83,7 +78,7 @@ class ShopIA:
         size_obj, thickness_obj = 0.60, 1
 
         # Add shopping list with price
-        # Bolso
+        # Dona
         if object == 'donut' not in [item[0] for item in self.shopping_list]:
             price = list_products['donut']
             self.shopping_list.append([object, price])
@@ -96,7 +91,7 @@ class ShopIA:
             # Price
             self.accumulative_price = self.accumulative_price + price
 
-        # Pelota
+        # Telefono
         if object == 'cell phone' not in [item[0] for item in self.shopping_list]:
             price = list_products['cell phone']
             self.shopping_list.append([object, price])
